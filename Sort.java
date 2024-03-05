4deddbdd327a162880509a9e27ec64923274e434
Qstrich <128658949+Qstrich@users.noreply.github.com>
2024-03-05 18:23:49 +0000

    com

--- Sort.java
+++ Sort.java
@@ -1,8 +1,39 @@
+/* Name: Jason Medeiros
+ * Course: ICS4U1
+ * Teacher: Ms.Lam
+ * Purpose: Implement various sorting algorithms
+ */
+
 public class Sort {
     public static void main(String[] args) {
         int[] a = {5, 8, 2, 9, 3, 6};
-        bubbleSort(a);
+        cocktailSort(a);
         for (int i: a) System.out.print(i + " "); System.out.println();
+    }
+    static void cocktailSort(int[] a) {
+        boolean sorted = false;
+        int l = 0, r = a.length;
+        while (!sorted) {
+            sorted = true;
+            for (int i = l; i < r-1; i++) {
+                if (a[i] > a[i+1]) {
+                    sorted = false;
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                }
+            }
+            for (int i = r-1; i > l; i--) {
+                if (a[i] < a[i-1]) {
+                    sorted = false;
+                    int temp = a[i];
+                    a[i] = a[i-1];
+                    a[i-1] = temp;
+                }
+            }
+            l++;
+            r--;
+        }
     }
     static void insertionSort(int[] a) {
         for (int i = 1; i < a.length; i++) {
