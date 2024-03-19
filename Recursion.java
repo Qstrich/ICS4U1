9b88e4ac8274d20f4c98cede06c98eb2556777e0
Qstrich <128658949+Qstrich@users.noreply.github.com>
2024-03-19 01:41:32 +0000

    c

---
+++ Recursion.java
@@ -0,0 +1,23 @@
+class Recursion {
+    public static void main(String[] args) {
+        
+    }   
+    static int gcd(int n, int m) {
+        if (n == m) return m;
+        if (m > n) return gcd(n, m-n);
+        if (m < n) return gcd(n, m);
+    } 
+    static int square(int x) {
+        if (x == 1) return 1;
+        return square(x-1) + 2 * x - 1;
+    }
+    static boolean primeNumber(int x) {
+        return prime(x, x-1);
+    }
+    static boolean prime(int x, int y) {
+        if (y == 1) return true;
+        if (x % y == 0) return false;
+        return prime(x, y-1);
+    }
+
+}
