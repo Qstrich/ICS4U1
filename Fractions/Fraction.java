d66766bf5862b6d827970b283f3dfb43abfb7f05
Qstrich <128658949+Qstrich@users.noreply.github.com>
2024-04-09 17:30:41 +0000

    c

--- Fractions/Fraction.java
+++ Fractions/Fraction.java
@@ -1,24 +1,41 @@
 package Fractions;
 
 public class Fraction {
-    int num;
-    int dem;
-    public class Fraction (int num, int dem) {
+    private int num;
+    private int den;
+    public class Fraction () {}
+    public class Fraction (int num, int den) {
         this.num = num;
-        this.dem = dem;
+        this.dem = den;
     }
+    public class Fraction (Fraction f) {
+        this.num = f.getNum();
+        this.den = f.getDen();
+    }
+    public class Fraction (double d) {
+        int dec = 1;
+        while (d * dec != (int) d) {
+            dec *= 10;
+        }
+        num = d * dec;
+        den = dec;
+    }
+    public int getNum() { return num; }
+    public int getDen() { return den; }
+    public void setNum(int num) { this.num = num }
+    public void setDen(int den) { this.den = den }
     public double size() {
-        return Math.abs((double) num/dem);
+        return Math.abs((double) num / den);
     }
     public Fraction plus(Fraction f) {
         Fraction sum = new Fraction();
-        sum.dem = f.dem * dem;
-        sum.num = f.dem * num + f.num * dem;
+        sum.setDen(f.getDen() * den);
+        sum.setNum(f.getDen() * num + f.getNum() * den);
         return sum;
     }
     public Fraction reduce() {
-        num = num / gcd(num, dem);
-        dem = dem / gcd(num, dem);
+        num = num / gcd(num, den);
+        den = den / gcd(num, den);
         return this;
     }
     private static void gcd(int n, int m) {
